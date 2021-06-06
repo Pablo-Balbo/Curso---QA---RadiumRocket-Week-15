386e41602adfa6933b35828a54273f436eb73f68
Pablo <contact@fastweb.com>
2021-06-06 19:44:27 -0300

    modified file paths

--- test/specs/e2eTests/e2eTest.standardUser.js
+++ test/specs/e2eTests/e2eTest.standardUser.js
@@ -1,9 +1,9 @@
-const LoginPage = require('../pageobjects/login.page');
-const InventoryPage = require('../pageobjects/inventory.page');
-const CartPage = require('../pageobjects/cart.page');
-const CheckoutStepOnePage = require('../pageobjects/checkoutStepOne.page');
-const CheckoutStepTwoPage = require('../pageobjects/checkoutStepTwo.page');
-const CheckoutCompletePage = require('../pageobjects/checkoutComplete.page');
+const LoginPage = require('../../pageobjects/login.page');
+const InventoryPage = require('../../pageobjects/inventory.page');
+const CartPage = require('../../pageobjects/cart.page');
+const CheckoutStepOnePage = require('../../pageobjects/checkoutStepOne.page');
+const CheckoutStepTwoPage = require('../../pageobjects/checkoutStepTwo.page');
+const CheckoutCompletePage = require('../../pageobjects/checkoutComplete.page');
 
 describe('End to end test - Standard User', () => {
     it('Make a purchase end to end', () => {
